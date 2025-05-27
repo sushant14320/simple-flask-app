ea64a8f2769e6c874518a2670408ade1afbbee7a
sushant14320 <51773383+sushant14320@users.noreply.github.com>
2025-05-27 13:50:02 +0000

    changes in code files

---
+++ test.py
@@ -0,0 +1,8 @@
+from sample_app.main import app
+
+
+def test_health():
+    with app.test_client() as client:
+        response = client.get('/health')
+        assert response.status_code == 200
+        assert response.json == {'status': 'app is running'}
